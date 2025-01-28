333f523aa0d7afb66164d4e063aed5da91bf4f16
aslibaltaci <64415581+aslibaltaci@users.noreply.github.com>
2025-01-28 00:44:41 +0000

    MainMenu update

---
+++ Innerverse/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Play()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+
+    // Update is called once per frame
+    public void Quit()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
